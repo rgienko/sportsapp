a9048866fbc1907ab59ab7cd3f319170cc9ed2fe
rgienko <rgienko@gmail.com>
2022-04-27 21:28:44 -0500

    Design of hockey main page Created function to get hockey games for a specific date

--- hockey/views.py
+++ hockey/views.py
@@ -8,15 +8,31 @@ from tzlocal import get_localzone
 
 
 # Create your views here.
+def get_today_minus_two_days():
+    today_minus_two_days = date.today() - timedelta(days=2)
+    return today_minus_two_days
 
-def index(request):
+
+def get_today_minus_one_day():
+    today_minus_one_day = date.today() - timedelta(days=1)
+    return today_minus_one_day
+
+
+def get_today_plus_two_days():
+    today_plus_two_days = date.today() + timedelta(days=2)
+    return today_plus_two_days
+
+
+def get_today_plus_one_day():
+    today_plus_one_day = date.today() + timedelta(days=1)
+    return today_plus_one_day
+
+
+def getHockeyScores(dt):
     url = "https://v1.hockey.api-sports.io/"
 
     payload = {}
-
     get_what = 'games'
-    dt = date.today()
-    print(str(dt))
     timezone = get_localzone()
 
     headers = {
@@ -24,38 +40,24 @@ def index(request):
         'x-rapidapi-key': "8cda7f9e3ef5b2dc538fca8432320b5d"
     }
 
-    # response = requests.request("GET", url + get_what + "?date=" + str(dt) + "&timezone=" + str(timezone),
-    #                           headers=headers, data=payload)
-
-    response = requests.request("GET",
-                                url + get_what + "?date=" + str(dt) + "&timezone=America/Chicago"
-                                + "&league=" + str(57) + "&season=" + str(2021),
-                                headers=headers, data=payload)
+    response = requests.request("GET", url + get_what + "?date=" + str(dt) + "&timezone=" + str(timezone)
+                                + "&league=" + str(57) + "&season=" + str(2021), headers=headers, data=payload)
 
     hockey_games = response.json()['response']
-    print(str(hockey_games))
+    return hockey_games
+
+
+def index(request):
+    today = date.today()
+    hockey_games = getHockeyScores(today)
 
     return render(request, '../templates/hockeyv2.html',
                   {
-                      'response': response,
-                      'hockey_games': hockey_games
+                      'hockey_games': hockey_games,
+                      'today': today,
+                      'today_m2': get_today_minus_two_days(),
+                      'today_m1': get_today_minus_one_day(),
+                      'today_p1': get_today_plus_one_day(),
+                      'today_p2': get_today_plus_two_days()
 
                   })
-
-
-def getTimeZone():
-    import http.client
-
-    conn = http.client.HTTPSConnection("v1.hockey.api-sports.io")
-
-    headers = {
-        'x-rapidapi-host': "v1.hockey.api-sports.io",
-        'x-rapidapi-key': "8cda7f9e3ef5b2dc538fca8432320b5d"
-    }
-
-    conn.request("GET", "/timezone", headers=headers)
-
-    res = conn.getresponse()
-    data = res.read()
-
-    print(data.decode("utf-8"))
